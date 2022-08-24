24cb603218ea11c4a53f5cfdd0aabb36bf48bcdc
AnkitAnkushe <ankitankushe@gmail.com>
2022-08-24 17:12:59 +0530

    added medium project

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './project.css'
+import medium from '../Items/Medium.png'
 import asos from '../Items/Asos.png'
 import fitness1 from '../Items/fitness1.png'
 import freshly1 from '../Items/freshly1.png'
@@ -9,6 +10,28 @@ const project = () => {
     <section id='project'>
       <h2 id="projectHeading">Projects</h2>
       <div className="container project_container">
+        {/* Medium */}
+        <article className='project_item'>
+          <div className="project_item-image">
+            <img src={medium} alt="" />              
+          </div>
+          <div>
+          <h2>Medium Clone</h2>
+          <h3>About</h3>
+         <li>-This is a Bloging Website </li>
+         <li>-Here user can read, write, edit, delete, and upload blogs </li>
+          <li>-Collaborative Project with the team of four members in the duration of one week.</li>
+          <li><b>Tech Stack</b>: HTML, CSS, JS, React, Redux, Material-UI</li>
+          <h3>My Part</h3>
+          <li>-Worked on Authentication</li>
+          <li>-Worked on Home page</li>
+          <li>-Worked on Payment page</li>
+         <div className="project_item-button">
+         <a href="https://github.com/theatulanand/medium-clone" className='btn' target="_blank">Github </a>
+          <a href="https://medium-clone-masai.netlify.app/" className='btn' target="_blank">Live </a>
+         </div>
+          </div>
+        </article>
 
         {/* Asos */}
         <article className='project_item'>
@@ -19,7 +42,7 @@ const project = () => {
           <h2>Asos Clone</h2>
           <h3>About</h3>
          <li>-This is a E-commerce Website of different kind of men and women Clothes.</li>
-          <li>-Collaborative Project with the team size of four members in the duration of one week.</li>
+          <li>-Collaborative Project with the team of four members in the duration of one week.</li>
           <li><b>Tech Stack</b>: HTML, CSS, JS, React, Redux, Material-UI</li>
           <h3>My Part</h3>
           <li>-Worked on Home Page</li>
@@ -41,6 +64,7 @@ const project = () => {
           <h3>About</h3>
           <li>-It is a Fitness App</li>
           <li>-Keeps check on your daily fitness routine</li>
+          <li>-Collaborative Project with the team of four members in the duration of one week.</li>
           <li><b>Tech Stack</b>: HTML, CSS, JS, Json-Server</li>
           <h3>My Part</h3>
           <li>-Worked on Exercise page</li>
@@ -62,6 +86,7 @@ const project = () => {
          <h3>About</h3>
          <li>-It is a Food delivery app</li>
           <li>-Has vast variety of foods</li>
+          <li>-Collaborative Project with the team of four members in the duration of one week.</li>
           <li><b>Tech Stack</b>: HTML, CSS, JS</li>
           <h3>My Part</h3>
           <li>-Worked on Menu page</li>
